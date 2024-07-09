326a7e97aa55d73229caf900a4bb484171ada7f5
bsswd <alexandersinkin@mail.ru>
2024-07-09 14:06:26 +0700

    Initial commit

---
+++ Source/OTUS_Learn.Target.cs
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class OTUS_LearnTarget : TargetRules
+{
+	public OTUS_LearnTarget(TargetInfo Target) : base(Target)
+	{
+		Type = TargetType.Game;
+		DefaultBuildSettings = BuildSettingsVersion.V5;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_4;
+		ExtraModuleNames.Add("OTUS_Learn");
+	}
+}
---
+++ Source/OTUS_Learn/OTUS_LearnGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "OTUS_LearnGameMode.h"
+#include "OTUS_LearnCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AOTUS_LearnGameMode::AOTUS_LearnGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPerson/Blueprints/BP_ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
